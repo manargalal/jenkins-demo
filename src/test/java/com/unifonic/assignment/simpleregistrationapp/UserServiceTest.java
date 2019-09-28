ecaa976346f657f348f4ebf39547705bb705e80b
manaranan <manar.anan@beetleware.com>
2019-09-28 22:48:32 +0200

    testupdate

--- src/test/java/com/unifonic/assignment/simpleregistrationapp/UserServiceTest.java
+++ src/test/java/com/unifonic/assignment/simpleregistrationapp/UserServiceTest.java
@@ -37,7 +37,7 @@ public class UserServiceTest {
 	@Test
 	public void creatUser_Successfully_In_Users_Table() throws Exception{
 		Users newUser = new Users();
-		newUser.setFirstName("manarrrrr");
+		newUser.setFirstName("manarrr");
 		newUser.setLastName("anan");
 		newUser.setEmail("manar@gmail.com");
 		newUser.setPhoneNumber("962799999999");
